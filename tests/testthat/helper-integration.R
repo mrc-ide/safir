ccd1cc795c04166b4f5d9616c1c47892f0247508
Peter Winskill <p.winskill@imperial.ac.uk>
2021-03-29 09:28:00 +0100

    Update helpers for mocking in tests

--- tests/testthat/helper-integration.R
+++ tests/testthat/helper-integration.R
@@ -1,34 +1,48 @@
-#' @title api helper function, mock_api
-#'
-#' @param values values
-#' @param parameters list pf parameters
-#' @param timestep time step
-#'
-#' @return api
-#'
-#' @export
-mock_api <- function(values, parameters = list(), timestep = 1) {
+mock_category <- function(...) {
+  v <- individual::CategoricalVariable$new(...)
   list(
-    get_state = function(individual, ...) {
-      subset <- NULL
-      for (state in list(...)) {
-        subset <- c(subset, values[[individual$name]][[state$name]])
-      }
-      subset
-    },
-    get_variable = function(individual, variable, index=NULL) {
-      if (!is.null(index)) {
-        return(values[[individual$name]][[variable$name]][index])
-      }
-      values[[individual$name]][[variable$name]]
-    },
-    queue_state_update = mockery::mock(),
-    queue_variable_update = mockery::mock(),
-    schedule = mockery::mock(),
-    clear_schedule = mockery::mock(),
-    get_scheduled = mockery::mock(),
-    get_timestep = function() timestep,
-    get_parameters = function() parameters,
+    get_index_of = v$get_index_of,
+    get_size_of = v$get_size_of,
+    queue_update = mockery::mock()
+  )
+}
+
+mock_double <- function(...) {
+  v <- individual::DoubleVariable$new(...)
+  list(
+    get_values = v$get_values,
+    queue_update = mockery::mock()
+  )
+}
+
+mock_integer <- function(...) {
+  v <- individual::IntegerVariable$new(...)
+  list(
+    get_values = v$get_values,
+    queue_update = mockery::mock()
+  )
+}
+
+mock_render <- function(...) {
+  v <- individual::Render$new(...)
+  list(
     render = mockery::mock()
   )
+}
+
+mock_event <- function(event) {
+  list(
+    get_scheduled = function(...) event$get_scheduled(...),
+    schedule = mockery::mock(),
+    clear_schedule = mockery::mock()
+  )
+}
+
+expect_bitset_update <- function(mock, value, index, call = 1) {
+  expect_equal(mockery::mock_args(mock)[[call]][[1]], value)
+  if("Bitset" %in% is(mockery::mock_args(mock)[[call]][[2]])){
+    expect_equal(mockery::mock_args(mock)[[call]][[2]]$to_vector(), index)
+  } else {
+    expect_equal(mockery::mock_args(mock)[[call]][[2]], index)
+  }
 }
