67e490e4ef7e67854ffb0dbe2f72c5fbbbb54da8
Peter Winskill <p.winskill@imperial.ac.uk>
2021-03-29 09:28:12 +0100

    update event testing

--- tests/testthat/test-events.R
+++ tests/testthat/test-events.R
@@ -12,182 +12,123 @@ test_that("create_event_based_processes assigns a listener to each event", {
     tt_contact_matrix = 0
   )
 
-  events <- create_events()
-  states <- create_states(psq)
+  events <- create_events(psq)
   variables <- create_variables(pop, psq)
-  human <- create_human(states, variables, events)
-
-  create_event_based_processes(
-    human,
-    states,
+  attach_event_listeners(
     variables,
     events,
     psq
   )
 
   for (event in events) {
-    expect_gt(length(event$listeners), 0)
+    expect_gt(length(event$.listeners), 0)
   }
 })
 
 test_that("test create_infection_update_listener", {
 
-  human <- mockery::mock()
-  IMild <- mockery::mock()
+  variables$states <- mock_category(c("S", "IMild"), rep("S", 5))
 
   ret <- create_infection_update_listener(
-    human,
-    IMild
+    variables,
+    "IMild"
   )
 
-  api <- list(queue_state_update = mockery::mock())
-  to_move <- mockery::mock()
-  ret(api, to_move)
-  mockery::expect_args(api$queue_state_update, 1, human, IMild, to_move)
+  to_move <- individual::Bitset$new(5)
+  to_move <- to_move$insert(1)
+
+  ret(0, to_move)
+  mockery::expect_args(variables$states$queue_update, 1, "IMild", to_move)
 
 })
 
 test_that("test create_progression_listener", {
 
-  events <- list(exposure = mockery::mock())
-  shift <- 0
-  duration <- 1
-  target <- mockery::mock()
-  func_mock <- mockery::mock()
-  r_erlang_mock <- mockery::mock(c(TRUE, TRUE, TRUE, TRUE))
+  # Without shift
+  events$exposure <- mock_event()
+  ret <- create_progression_listener(events$exposure, duration = 1, shift = 0, func = r_erlang)
+  # Stub so that delays = 1:4
+  mockery::stub(r_erlang, "rgamma", 1:4)
 
-  ret <- create_progression_listener(
-    events$exposure,
-    duration,
-    shift,
-    func_mock
-  )
+  to_move <- individual::Bitset$new(4)
+  to_move <- to_move$insert(1:4)
+  ret(1, to_move)
 
-  api <- list(schedule = mockery::mock())
+  mockery::expect_args(events$exposure$schedule, 1, to_move, 1:4)
 
-  mockery::stub(ret, 'r_erlang', mockery::mock(c(TRUE, TRUE, TRUE, TRUE)))
+  # With shift
+  events$exposure <- mock_event()
+  ret <- create_progression_listener(events$exposure, duration = 1, shift = 1, func = r_erlang)
+  mockery::stub(r_erlang, "rgamma", 1:4)
 
-  with_mock(
-    'safir::r_erlang' = r_erlang_mock,
-    ret2 <- api$schedule(
-      events$exposure,
-      target,
-      rep(.5, 4)
-    )
-  )
+  to_move <- individual::Bitset$new(4)
+  to_move <- to_move$insert(1:4)
+  ret(1, to_move)
 
-  ret(api, target)
-
-  mockery::expect_args(
-    api$schedule,
-    1,
-    event = events$exposure,
-    target = target,
-    func_mock = c(0.5, 0.5, 0.5, 0.5)
-  )
-
-  mockery::expect_called(api$schedule, 2)
+  mockery::expect_args(events$exposure$schedule, 1, to_move, 1:4 + 1)
 
 })
 
 test_that("test create_exposure_update_listener", {
 
-  human <- mockery::mock()
-  states <- mockery::mock()
+  events$severe_infection <- mock_event()
+  events$mild_infection <- mock_event()
+  events$asymp_infection <- mock_event()
+
+
 
   ret <- create_exposure_update_listener(
-    human,
-    states,
     events,
     variables,
-    parameters
+    psq
   )
-
-  api <- list(get_variable = mockery::mock(), schedule = mockery::mock())
-
-  variables <- list(discrete_age = mockery::mock())
-
-  events <- list(exposure = mockery::mock(),
-                 mild_infection = mockery::mock(),
-                 asymp_infection = mockery::mock(),
-                 severe_infection = mockery::mock())
-
-  problist <- seq(1,17,1)
-  exposure = mockery::mock()
-
-  parameters <- list(dur_E = mockery::mock(),
-                     prob_hosp = list(mockery::mock(problist)),
-                     prob_asymp = list(mockery::mock(problist)))
-
-  to_move <- c(6, 7, 3, 2, 5, 8)
-
-  mockery::stub(ret, 'r_erlang', mockery::mock(c(0.5, 0.5), c(0.4, 0.4),
-                                               c(0.7, 0.7)))
-
+  # Stub delays
+  mockery::stub(ret, "r_erlang", mockery::mock(c(0.5, 0.5), c(0.4, 0.4), c(0.7, 0.7)))
+  # Stub tree probs
   mockery::stub(ret, 'bernoulli_multi_p',
                 mockery::mock(c(TRUE, TRUE, FALSE, FALSE, FALSE, FALSE),
                               c(TRUE, TRUE, FALSE, FALSE)))
-  ret(api, to_move)
 
-  # The process should have called api$schedule three times
 
-  # First call is to schedule the severe infection for individuals 6, 7
-  # who schedule in 0 days
+  to_move <- individual::Bitset$new(6)
+  to_move <- to_move$insert(1:6)
+
+  ret(0, to_move)
+
+  # The process should have called schedule three times
+
+  # First call is to schedule the severe infection for individuals 1 & 2
+  # who schedule in 0.5 + 1 days
+  have_move1 <- individual::Bitset$new(10)
+  have_move1 <- to_move$insert(1:2)
   mockery::expect_args(
-    api$schedule,
+    events$severe_infection$schedule,
     1,
-    event = events$severe_infection,
-    target = c(6, 7),
-    delay = c(1.5, 1.5)
+    have_move1,
+    c(1.5, 1.5)
   )
 
-  # The second call is to schedule the mild infection for individuals 3, 2
-  # who schedule in 0 days
+  # The second call is to schedule the asymptomatic infection for individuals
+  # 5 & 6 who schedule in 0.4 + 1 days
+  have_move3 <- individual::Bitset$new(10)
+  have_move3 <- to_move$insert(5:6)
   mockery::expect_args(
-    api$schedule,
-    2,
-    event = events$mild_infection,
-    target = c(3, 2),
-    delay = c(1.4, 1.4)
+    events$asymp_infection$schedule,
+    1,
+    have_move3,
+    c(1.4, 1.4)
   )
 
-  # The third call is to schedule the asymptomatic infection for individuals
-  # 5, 8 who schedule in 0 days
+  # The thirs call is to schedule the mild infection for individuals 3 & 4
+  # who schedule in 0.7 + 1 days
+  have_move2 <- individual::Bitset$new(10)
+  have_move2 <- to_move$insert(3:4)
   mockery::expect_args(
-    api$schedule,
-    3,
-    event = events$asymp_infection,
-    target = c(5, 8),
-    delay = c(1.7, 1.7)
+    events$mild_infection$schedule,
+    1,
+    have_move2,
+    c(1.7, 1.7)
   )
-
 })
 
 
-test_that("test initialise_progression", {
-
-  event <- mockery::mock()
-  human <- mockery::mock()
-  from_state <- mockery::mock()
-  duration <- 1
-  r_erlang_mock <- mockery::mock(c(TRUE, TRUE, TRUE, TRUE))
-
-  ret <- initialise_progression(event, human, from_state, duration)
-
-  api <- list(schedule = mockery::mock(), get_state = mockery::mock())
-  target <- mockery::mock()
-
-  ret(api, target)
-
-  mockery::expect_args(api$get_state, 1, human, from_state)
-
-  with_mock(
-    'safir::r_erlang' = r_erlang_mock,
-    ret3 <- api$schedule(
-      event,
-      target,
-      rep(.5, 4)
-    )
-  )
-
-})
