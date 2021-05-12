4a7f6cc0c025cec147011d1461aa43bf2c7dac04
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-05-12 16:16:54 -0700

    geometric approximation fn

--- R/utils.R
+++ R/utils.R
@@ -18,6 +18,25 @@ r_erlang <- function(size, mu) { rgamma(size, shape = 2, rate = 2 / mu) }
 #' @importFrom stats rexp
 r_exp <- function(size, mu) { rexp(size, rate = 1 / mu) }
 
+#' Make geometric waiting time distribution
+#'
+#' @details Random draws from a geometric distribution
+#' which approximates a continuous time exponential distribution.
+#' See [pomp documentation](https://kingaa.github.io/pomp/vignettes/C_API.html#exponentialgeometric-rate-conversion)
+#' for details on the approximation.
+#' @param mu Mean duration
+#' @param dt Size of time step
+#' @importFrom stats rgeom pexp
+make_rexp <- function(mu, dt) {
+  assert_pos(mu, zero_allowed = FALSE)
+  assert_pos(dt, zero_allowed = FALSE)
+  R <- 1 / mu
+  r <- log(1 + (R*dt)) / dt
+  function(n) {
+    rgeom(n = n, prob = pexp(q = r * dt)) + 1L
+  }
+}
+
 #' @noRd
 remove_non_numerics <- function(l) {
   clean <- list()
