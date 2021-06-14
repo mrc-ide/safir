2715a56f7317377c0e71123c66c92305ad3feb30
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-06-14 12:30:49 -0700

    test vaxx coverage

--- tests/testthat/test-vaccination.R
+++ tests/testthat/test-vaccination.R
@@ -1,3 +1,38 @@
+test_that("coverage and get_proportion_vaccinated are giving the same results", {
+  dose_times <- list(matrix(c(1, 2, NA, 2, 3, NA), nrow = 3),
+                     matrix(c(NA, NA, NA, 2, 3, 4), nrow = 3),
+                     matrix(c(1, 2, 2, NA, NA, NA), nrow = 3))
+
+  dose_1 <- unlist(lapply(dose_times,function(x){x[,1]}))
+  dose_1[which(is.na(dose_1))] <- -1
+
+  dose_2 <- unlist(lapply(dose_times,function(x){x[,2]}))
+  dose_2[which(is.na(dose_2))] <- -1
+
+  variables <- list()
+  variables$dose_time <- list()
+  variables$dose_time[[1]] <- IntegerVariable$new(dose_1)
+  variables$dose_time[[2]] <- IntegerVariable$new(dose_2)
+  variables$discrete_age <- IntegerVariable$new(rep(1:length(dose_times),times=sapply(dose_times,nrow)))
+
+  cov_safir <- sapply(X = 1:length(dose_times),FUN = function(a){
+    get_proportion_vaccinated(variables = variables,age = a,dose = 1)
+  })
+
+  cov_safir_2 <- sapply(X = 1:length(dose_times),FUN = function(a){
+    get_proportion_vaccinated(variables = variables,age = a,dose = 2)
+  })
+
+  expect_equal(
+    cov_safir,
+    nimue:::coverage(dose_times, 1)
+  )
+  expect_equal(
+    cov_safir_2,
+    nimue:::coverage(dose_times, 2)
+  )
+})
+
 test_that('eligable_for_second and eligible_for_dose_vaccine give equivalent results with same input', {
   dose_times <- list(matrix(c(1, 2, NA, NA, 3, NA), nrow = 3),
                      matrix(c(NA, NA, NA, NA, 3, 4), nrow = 3),
