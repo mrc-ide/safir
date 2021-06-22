3b3cd51b7dd1eddfeea281c2cfe1d6d7fd5daedb
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-06-22 14:59:10 -0700

    unneeded tests for types

--- tests/testthat/test-phase-vaccination.R
+++ tests/testthat/test-phase-vaccination.R
@@ -1,12 +1,16 @@
 
 
-test_that("testing get_vaccination_priority_stage", {
-
-  parameters <- list()
-  parameters$vaccine_coverage_mat <- nimue::strategy_matrix(strategy = "Elderly")
-  parameters$N_age <- nrow(parameters$vaccine_coverage_mat)
-  parameters$N_phase <- 3
-
-  get_vaccination_priority_stage
-
-})
+# test_that("testing get_vaccination_priority_stage", {
+#
+#   parameters <- list()
+#   parameters$vaccine_coverage_mat <- nimue::strategy_matrix(strategy = "Elderly")
+#   parameters$N_age <- nrow(parameters$vaccine_coverage_mat)
+#   parameters$N_phase <- 3
+#
+#   # prioritize 3 oldest age groups
+#   parameters$next_dose_priority <- matrix(0,nrow = 2, ncol = 17)
+#   parameters$next_dose_priority[1:2, 15:17] <- 1
+#
+#   get_vaccination_priority_stage
+#
+# })
