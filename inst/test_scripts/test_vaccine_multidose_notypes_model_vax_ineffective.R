7353e02c70253bea0e1b9f7bbe9ed7cac6514d05
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-07-20 17:41:20 -0700

    include tests of ineffective vs. effective vaccine

--- inst/test_scripts/test_vaccine_multidose_notypes_model_vax_ineffective.R
+++ inst/test_scripts/test_vaccine_multidose_notypes_model_vax_ineffective.R
@@ -1,5 +1,6 @@
 # --------------------------------------------------------------------------------
 #   test vaxx model, multi dose, no types
+#   completely ineffective vaccine
 # --------------------------------------------------------------------------------
 
 rm(list=ls());gc()
@@ -19,12 +20,11 @@ R0 <- 4
 # vaccine dosing
 vaccine_doses <- 2
 dose_period <- c(NaN, 28)
-vaccine_set <- c(0, seq(from = 1e3, to = 1e4, length.out = (tmax/dt)-1))
+vaccine_set <- c(0, rep(1e4, times = (tmax/dt)-1))
 vaccine_set <- floor(vaccine_set)
-# vaccine_set <- 0*vaccine_set
 
 # vaccine strategy
-vaccine_coverage_mat <- strategy_matrix(strategy = "Elderly",max_coverage = 0.5)
+vaccine_coverage_mat <- strategy_matrix(strategy = "Elderly",max_coverage = 0.8)
 next_dose_priority <- matrix(data = 0, nrow = vaccine_doses - 1,ncol = ncol(vaccine_coverage_mat))
 next_dose_priority[1, 15:17] <- 1 # prioritize 3 oldest age groups for next dose
 storage.mode(next_dose_priority) <- "integer"
@@ -37,9 +37,6 @@ parameters <- safir::get_parameters(
   R0 = R0,
   time_period = tmax
 )
-# parameters$IAsymp_0[1:17] <- 3 # some additional infectives at t=0
-# parameters$population <- parameters$population + 3
-# pop$n <- parameters$population # fix this. we don't want to use this twice.
 
 # attach vaccine parameters (roll into function eventually)
 parameters$N_prioritisation_steps <- nrow(vaccine_coverage_mat)
@@ -52,6 +49,9 @@ parameters$N_phase <- vaccine_doses
 
 # attach Ab dynamics parameters
 ab_parameters <- get_vaccine_ab_titre_parameters(vaccine = "Pfizer", max_dose = vaccine_doses)
+# super effective
+ab_parameters$ab_50 <- 2e-12
+ab_parameters$ab_50_severe <- 2e-12
 parameters <- c(parameters, ab_parameters)
 
 # create variables
@@ -86,7 +86,7 @@ processes <- list(
   infection_process_vaccine(parameters = parameters,variables = variables,events = events,dt = dt),
   categorical_count_renderer_process_daily(renderer = renderer,variable = variables$states,categories = variables$states$get_categories(),dt = dt),
   double_count_render_process_daily(variable = variables$ab_titre,dt = dt),
-  integer_count_render_process_daily(renderer = dose_renderer,variable = variables$dose_num,margin = 0:2,dt = dt)
+  integer_count_render_process_daily(renderer = dose_renderer,variable = variables$dose_num,margin = 0:vaccine_doses,dt = dt)
 )
 
 setup_events_vaccine(parameters = parameters,events = events,variables = variables,dt = dt)
@@ -98,9 +98,6 @@ system.time(simulation_loop_vaccine(
   timesteps = timesteps,
   TRUE
 ))
-
-
-
 
 # plot: ab titre
 vaccinated <- variables$dose_num$get_index_of(set = 0)
