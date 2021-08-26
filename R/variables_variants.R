6465cf186b8350c90dc2c790c1381c606f7c43d2
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-08-26 14:07:37 -0700

    categoricalvariable for voc and update

--- R/variables_variants.R
+++ R/variables_variants.R
@@ -5,9 +5,6 @@
 # --------------------------------------------------
 
 
-
-
-
 #' @title Create variant of concern variables
 #' @description Please note the variant model assumes the user is applying
 #' the vaccine model with antibody dynamics (not nimue or squire versions).
@@ -19,12 +16,21 @@
 create_voc_variables <- function(variables, parameters) {
 
   stopifnot(all(c("wt","none") %in% parameters$voc_types))
-
   n <- sum(parameters$population)
 
+  # this tracks what a person's current infection is
   variables$voc <- CategoricalVariable$new(categories = parameters$voc_types,initial_values = rep("none", n))
 
-
-
   return(variables)
+}
+
+#' @title Update variant of concern variables
+#' @description This should be called from the simulation loop. It does not
+#' update disease state.
+#' @param variables a list
+#' @export
+update_voc_variables <- function(variables) {
+
+  variables$voc$.update()
+
 }
