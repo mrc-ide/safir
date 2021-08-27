4e51dd79b6730c09e5149191a99188080906ed98
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-08-26 21:51:39 -0700

    testing for R and C++ efficacy functions

--- src/efficacy_vaccination.cpp
+++ src/efficacy_vaccination.cpp
@@ -15,18 +15,32 @@ std::vector<double> vaccine_efficacy_infection_cpp(
     const std::vector<double>& ab_titre,
     const Rcpp::List& parameters
 ) {
-  // nt <- exp(ab_titre)
-  std::vector<double> nt(ab_titre.size(), 0.0);
-  std::transform(ab_titre.cbegin(), ab_titre.cend(), nt.begin(), [](const double nt_i) -> double {return std::exp(nt_i);});
-
-  // ef_infection <- 1 / (1 + exp(-parameters$k * (log10(nt) - log10(parameters$ab_50)))) # reported efficacy in trials
-  // ef_infection <- 1 - ef_infection (in the return line)
   double k = Rcpp::as<double>(parameters["k"]);
   double ab_50 = Rcpp::as<double>(parameters["ab_50"]);
-  std::vector<double> ef_infection(ab_titre.size(), 0.0);
-  std::transform(nt.cbegin(), nt.cend(), ef_infection.begin(), [k, ab_50](const double a) -> double {
-    return 1.0 - (1.0 / (1.0 + std::exp(-k * (std::log10(a) - std::log10(ab_50)))));
-  });
+
+  std::vector<double> ef_infection(ab_titre.size(), 1.0);
+
+  for (auto i = 0u; i < ab_titre.size(); ++i) {
+    if (std::isfinite(ab_titre[i])) {
+      double nt = std::exp(ab_titre[i]);
+      ef_infection[i] = 1.0 - (1.0 / (1.0 + std::exp(-k * (std::log10(nt) - std::log10(ab_50)))));
+    } else {
+      continue;
+    }
+  }
+
+  // // nt <- exp(ab_titre)
+  // std::vector<double> nt(ab_titre.size(), 0.0);
+  // std::transform(ab_titre.cbegin(), ab_titre.cend(), nt.begin(), [](const double nt_i) -> double {return std::exp(nt_i);});
+  //
+  // // ef_infection <- 1 / (1 + exp(-parameters$k * (log10(nt) - log10(parameters$ab_50)))) # reported efficacy in trials
+  // // ef_infection <- 1 - ef_infection (in the return line)
+  // double k = Rcpp::as<double>(parameters["k"]);
+  // double ab_50 = Rcpp::as<double>(parameters["ab_50"]);
+  // std::vector<double> ef_infection(ab_titre.size(), 0.0);
+  // std::transform(nt.cbegin(), nt.cend(), ef_infection.begin(), [k, ab_50](const double a) -> double {
+  //   return 1.0 - (1.0 / (1.0 + std::exp(-k * (std::log10(a) - std::log10(ab_50)))));
+  // });
 
   // efficacy against infection
   return ef_infection;
@@ -45,21 +59,37 @@ std::vector<double> vaccine_efficacy_severe_cpp(
     const std::vector<double>& ef_infection,
     const Rcpp::List& parameters
 ) {
-  // nt <- exp(ab_titre)
-  std::vector<double> nt(ab_titre.size(), 0.0);
-  std::transform(ab_titre.cbegin(), ab_titre.cend(), nt.begin(), [](const double nt_i) -> double {return std::exp(nt_i);});
-
-  // ef_severe_uncond <- 1 / (1 + exp(-parameters$k * (log10(nt) - log10(parameters$ab_50_severe))))
-  // ef_severe <-  1 - ((1 - ef_severe_uncond)/(1 - ef_infection))
-  // ef_severe <- 1 - ef_severe
   double k = Rcpp::as<double>(parameters["k"]);
   double ab_50_severe = Rcpp::as<double>(parameters["ab_50_severe"]);
-  std::vector<double> ef_severe(ab_titre.size(), 0.0);
-  std::transform(nt.cbegin(), nt.cend(), ef_infection.cbegin(), ef_severe.begin(), [k, ab_50_severe](const double nt_i, const double ef_inf_i) -> double {
-    double ef_severe_uncond = 1.0 / (1.0 + std::exp(-k * (std::log10(nt_i) - std::log10(ab_50_severe))));
-    double ef_severe = 1.0 - ((1.0 - ef_severe_uncond)/(1.0 - ef_inf_i));
-    return 1.0 - ef_severe;
-  });
+
+  std::vector<double> ef_severe(ab_titre.size(), 1.0);
+
+  for (auto i = 0u; i < ab_titre.size(); ++i) {
+    if (std::isfinite(ab_titre[i])) {
+      double nt = std::exp(ab_titre[i]);
+      double ef_severe_uncond = 1.0 / (1.0 + std::exp(-k * (std::log10(nt) - std::log10(ab_50_severe))));
+      ef_severe[i] = 1.0 - ((1.0 - ef_severe_uncond)/(1.0 - ef_infection[i]));
+      ef_severe[i] = 1.0 - ef_severe[i];
+    } else {
+      continue;
+    }
+  }
+
+  // // nt <- exp(ab_titre)
+  // std::vector<double> nt(ab_titre.size(), 0.0);
+  // std::transform(ab_titre.cbegin(), ab_titre.cend(), nt.begin(), [](const double nt_i) -> double {return std::exp(nt_i);});
+  //
+  // // ef_severe_uncond <- 1 / (1 + exp(-parameters$k * (log10(nt) - log10(parameters$ab_50_severe))))
+  // // ef_severe <-  1 - ((1 - ef_severe_uncond)/(1 - ef_infection))
+  // // ef_severe <- 1 - ef_severe
+  // double k = Rcpp::as<double>(parameters["k"]);
+  // double ab_50_severe = Rcpp::as<double>(parameters["ab_50_severe"]);
+  // std::vector<double> ef_severe(ab_titre.size(), 0.0);
+  // std::transform(nt.cbegin(), nt.cend(), ef_infection.cbegin(), ef_severe.begin(), [k, ab_50_severe](const double nt_i, const double ef_inf_i) -> double {
+  //   double ef_severe_uncond = 1.0 / (1.0 + std::exp(-k * (std::log10(nt_i) - std::log10(ab_50_severe))));
+  //   double ef_severe = 1.0 - ((1.0 - ef_severe_uncond)/(1.0 - ef_inf_i));
+  //   return 1.0 - ef_severe;
+  // });
 
   return ef_severe;
 };
