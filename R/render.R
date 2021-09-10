07a8527a61b42ea1829ea91d3304b744637827b4
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-09-09 17:27:55 -0700

    render doses by age output daily

--- R/render.R
+++ R/render.R
@@ -43,6 +43,45 @@ integer_count_render_process_daily <- function(renderer, variable, margin, dt) {
   }
 }
 
+#' @title Render doses by age every day
+#' @description Renders the number of individuals with each dose in each age bin
+#' (cross tabulated doses and ages).
+#' This only renders output on timesteps that correspond to a day.
+#' See examples for how to quickly summarize the output.
+#' @param renderer a \code{\link[individual]{Render}} object
+#' @param age a \code{\link[individual]{IntegerVariable}} object
+#' @param dose a \code{\link[individual]{IntegerVariable}} object
+#' @param parameters model parameters
+#' @param dt size of time step
+#' @examples
+#' # if the rendered is called dose_age_renderer
+#' tmp <- as.data.table(dose_age_renderer$to_dataframe())
+#' tmp <- melt(tmp,id.vars="timestep")
+#' tmp1 <- tmp[, tstrsplit(variable, "_", keep = c(2, 4))]
+#' tmp[ , variable := NULL]
+#' tmp <- cbind(tmp, tmp1)
+#' setnames(x = tmp,old = c("V1", "V2"),new = c("dose","age"))
+#'
+#' dose_dt <- tmp[, .(value = sum(value)), by = .(dose, timestep)]
+#' @export
+dose_age_render_process_daily <- function(renderer, age, dose, parameters, dt) {
+  stopifnot(inherits(age, "IntegerVariable"))
+  stopifnot(inherits(dose, "IntegerVariable"))
+  stopifnot(inherits(renderer, "Render"))
+  num_dose <- parameters$N_phase
+  num_age <- parameters$N_age
+  function(t) {
+    if ((t * dt) %% 1 == 0) {
+      day <- as.integer(t * dt)
+      dose_age_tab <- cross_tab_doses_age(doses = dose$.variable, age = age$.variable, num_doses = num_dose, num_ages = num_age)
+      for (d in 1:nrow(dose_age_tab)) {
+        for (a in 1:ncol(dose_age_tab)) {
+          renderer$render(paste0("dose_", d-1, "_age_", a, "_count"), dose_age_tab[d, a], day)
+        }
+      }
+    }
+  }
+}
 
 #' @title Render categories every day
 #' @description Renders the number of individuals in each category, but only
