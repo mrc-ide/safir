fb27f566893f8d95784d999da590786e7b5267f7
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-11-18 17:39:07 -0800

    add tests confirming vaccine will block S->E (infection efficacy)

--- tests/testthat/test-processes-vaccination.R
+++ tests/testthat/test-processes-vaccination.R
@@ -55,182 +55,150 @@ test_that("c++ infection process (multiple doses, no types) returns identical re
 })
 
 
-test_that("c++ infection process (multiple doses, no types) testing with NAT affecting onward transmission", {
+test_that("infection process with vaccines: infection_efficacy prevents transmission (S->E) event queueing", {
 
   library(nimue)
+  library(individual)
 
-  # pars
-  iso3c <- "GBR"
-  pop <- safir::get_population(iso3c)
-  contact_mat <- squire::get_mixing_matrix(iso3c = iso3c)
-  pop$n <- as.integer(pop$n / 100)
+  # run 1: lots of vaccine
+
+  R0 <- 15
+  time_period <- 3
+  dt <- 1
+
+  pop <- get_population("GBR")
+  pop$n <- rep(1e3, 17) + rep(100, 17)
 
   parameters <- safir::get_parameters(
     population = pop$n,
-    contact_matrix_set = contact_mat,
-    iso3c = iso3c,
-    time_period = 365,
-    dt = 1
+    seeding_cases = 0,
+    iso3c = "GBR",
+    R0 = R0,
+    time_period = time_period,
+    dt = dt
   )
-  parameters$beta_set <- parameters$beta_set*rexp(n = length(parameters$beta_set))
+  parameters$S_0 <- parameters$S_0 - rep(100, 17)
+  parameters$ICase1_0 <- rep(100, 17)
 
-  # test states
-  n <- 1e5
-  dt <- 0.5
-  valid_states <- c("S","IMild","ICase","IAsymp")
-  state0 <- sample(x = valid_states,size = n,replace = T)
-  age0 <- sample.int(n = 17,size = n,replace = T)
-  vaccine_parameters <- get_vaccine_ab_titre_parameters(vaccine = "Pfizer")
-  ab_titre0 <- rep(-Inf, n)
-  parameters <- make_vaccine_parameters(safir_parameters = parameters,vaccine_ab_parameters = vaccine_parameters,vaccine_set = rep(100,365),dose_period = c(NaN, 10),strategy_matrix = nimue::strategy_matrix(strategy = "Elderly"),next_dose_priority_matrix = matrix(0,nrow = 1,ncol = 17))
+  # vaccine parameters
+  vaccine_coverage_mat <- nimue::strategy_matrix(strategy = "All",max_coverage = 0.95)
+  next_dose_priority <- matrix(nrow = 0, ncol = 17)
+  ab_parameters <- get_vaccine_ab_titre_parameters(vaccine = "Moderna", max_dose = 1,correlated = FALSE, max_ab = 10)
+  ab_parameters$mu_ab[1] <- 4
 
-  # standard (no NAT effect)
-  states <- individual::CategoricalVariable$new(categories = valid_states,initial_values = state0)
-  discrete_age <- individual::IntegerVariable$new(initial_values = age0)
-  ab_titre <- individual::DoubleVariable$new(initial_values = ab_titre0)
-  exposure <- individual::TargetedEvent$new(population_size = n)
+  vaccine_doses <- 1
+  dose_period <- NaN
 
-  set.seed(5436L)
-  inf_proc_no_nat <- infection_process_vaccine(parameters = parameters,variables = list(states=states,discrete_age=discrete_age,ab_titre=ab_titre),events = list(exposure=exposure),dt = dt)
-  inf_proc_no_nat(timestep = 100)
+  # vaccines
+  vaccine_set <- c(1e5, 1e5, 1e5)
 
-  no_nat <- exposure$get_scheduled()$to_vector()
+  # combine parameters and verify
+  parameters <- make_vaccine_parameters(
+    safir_parameters = parameters,
+    vaccine_ab_parameters = ab_parameters,
+    vaccine_set = vaccine_set,
+    dose_period = dose_period,
+    strategy_matrix = vaccine_coverage_mat,
+    next_dose_priority_matrix = next_dose_priority
+  )
 
-  # NAT effect (with no titre)
-  parameters$nt_efficacy_transmission <- TRUE
-  exposure <- individual::TargetedEvent$new(population_size = n)
+  # create variables
+  timesteps <- parameters$time_period/dt
+  variables <- create_variables(pop = pop, parameters = parameters)
+  variables <- create_vaccine_variables(variables = variables,parameters = parameters)
 
-  set.seed(5436L)
-  inf_proc_nat <- infection_process_vaccine(parameters = parameters,variables = list(states=states,discrete_age=discrete_age,ab_titre=ab_titre),events = list(exposure=exposure),dt = dt)
-  inf_proc_nat(timestep = 100)
+  # create events
+  events <- create_events(parameters = parameters)
+  events <- create_events_vaccination(events = events,parameters = parameters)
+  attach_event_listeners(variables = variables,events = events,parameters = parameters, dt = dt)
+  attach_event_listeners_vaccination(variables = variables,events = events,parameters = parameters,dt = dt)
 
-  nat_no_titre <- exposure$get_scheduled()$to_vector()
+  # make renderers
+  incidence_renderer <- individual::Render$new(timesteps)
 
-  expect_equal(no_nat, nat_no_titre)
+  # processes
+  processes <- list(
+    vaccine_ab_titre_process(parameters = parameters,variables = variables,dt = dt),
+    vaccination_process(parameters = parameters,variables = variables,events = events,dt = dt),
+    infection_process_vaccine_cpp(parameters = parameters,variables = variables,events = events,dt = dt)
+  )
 
-  # make sure significantly less infections occur when using NAT effect with a large ab titre
-  zdose <- log(10^rnorm(n = n, mean = log10(3), sd = parameters$std10))
-  ab_titre <- individual::DoubleVariable$new(initial_values = zdose)
-  exposure <- individual::TargetedEvent$new(population_size = n)
+  attach_tracking_listener_incidence(events = events, renderer = incidence_renderer)
+  setup_events(parameters = parameters,events = events,variables = variables,dt = dt)
 
-  set.seed(5436L)
-  inf_proc_nat <- infection_process_vaccine(parameters = parameters,variables = list(states=states,discrete_age=discrete_age,ab_titre=ab_titre),events = list(exposure=exposure),dt = dt)
-  inf_proc_nat(timestep = 100)
+  # no vaccination prior to sim
+  expect_equal(variables$dose_num$get_size_of(set = 0), sum(parameters$population))
 
-  nat_high_titre <- exposure$get_scheduled()$to_vector()
+  simulation_loop_safir(
+    variables = variables,
+    events = events,
+    processes = processes,
+    timesteps = timesteps,
+    variables_dont_update = c("discrete_age", "phase"),
+    progress = FALSE
+  )
 
-  expect_true(length(nat_high_titre) < length(nat_no_titre))
+  vaccine_run_incidence <- incidence_renderer$to_dataframe()
+
+  expect_true(variables$dose_num$get_size_of(set = 0) < sum(parameters$population))
+
+
+  # run 2 no vaccines
+
+  # vaccines
+  vaccine_set <- vaccine_set*0
+
+  # combine parameters and verify
+  parameters <- make_vaccine_parameters(
+    safir_parameters = parameters,
+    vaccine_ab_parameters = ab_parameters,
+    vaccine_set = vaccine_set,
+    dose_period = dose_period,
+    strategy_matrix = vaccine_coverage_mat,
+    next_dose_priority_matrix = next_dose_priority
+  )
+
+  # create variables
+  timesteps <- parameters$time_period/dt
+  variables <- create_variables(pop = pop, parameters = parameters)
+  variables <- create_vaccine_variables(variables = variables,parameters = parameters)
+
+  # create events
+  events <- create_events(parameters = parameters)
+  events <- create_events_vaccination(events = events,parameters = parameters)
+  attach_event_listeners(variables = variables,events = events,parameters = parameters, dt = dt)
+  attach_event_listeners_vaccination(variables = variables,events = events,parameters = parameters,dt = dt)
+
+  # make renderers
+  incidence_renderer <- individual::Render$new(timesteps)
+
+  # processes
+  processes <- list(
+    vaccine_ab_titre_process(parameters = parameters,variables = variables,dt = dt),
+    vaccination_process(parameters = parameters,variables = variables,events = events,dt = dt),
+    infection_process_vaccine_cpp(parameters = parameters,variables = variables,events = events,dt = dt)
+  )
+
+  attach_tracking_listener_incidence(events = events, renderer = incidence_renderer)
+  setup_events(parameters = parameters,events = events,variables = variables,dt = dt)
+
+  # no vaccination prior to sim
+  expect_equal(variables$dose_num$get_size_of(set = 0), sum(parameters$population))
+
+  simulation_loop_safir(
+    variables = variables,
+    events = events,
+    processes = processes,
+    timesteps = timesteps,
+    variables_dont_update = c("discrete_age", "phase"),
+    progress = FALSE
+  )
+
+  no_vaccine_run_incidence <- incidence_renderer$to_dataframe()
+
+  expect_equal(variables$dose_num$get_size_of(set = 0), sum(parameters$population))
+
+  # tests
+  expect_true(all(no_vaccine_run_incidence[-1, "incidence"] > vaccine_run_incidence[-1, "incidence"]))
 
 })
-
-
-test_that("R/C++ infection process consistent with NAT onward infectiousness and random draws of ab titre", {
-
-  library(nimue)
-
-  # pars
-  iso3c <- "GBR"
-  pop <- safir::get_population(iso3c)
-  contact_mat <- squire::get_mixing_matrix(iso3c = iso3c)
-  pop$n <- as.integer(pop$n / 100)
-
-  parameters <- safir::get_parameters(
-    population = pop$n,
-    contact_matrix_set = contact_mat,
-    iso3c = iso3c,
-    time_period = 365,
-    dt = 1
-  )
-  parameters$beta_set <- parameters$beta_set*rexp(n = length(parameters$beta_set))
-
-  # test states
-  n <- 1e5
-  dt <- 0.5
-  valid_states <- c("S","IMild","ICase","IAsymp")
-  state0 <- sample(x = valid_states,size = n,replace = T)
-  age0 <- sample.int(n = 17,size = n,replace = T)
-  vaccine_parameters <- get_vaccine_ab_titre_parameters(vaccine = "Pfizer")
-  parameters <- make_vaccine_parameters(safir_parameters = parameters,vaccine_ab_parameters = vaccine_parameters,vaccine_set = rep(100,365),dose_period = c(NaN, 10),strategy_matrix = nimue::strategy_matrix(strategy = "Elderly"),next_dose_priority_matrix = matrix(0,nrow = 1,ncol = 17))
-  parameters$nt_efficacy_transmission <- TRUE
-
-  ab_titre0 <- log(10^rnorm(n = n, mean = log10(1), sd = parameters$std10))
-
-  # R
-  states <- individual::CategoricalVariable$new(categories = valid_states,initial_values = state0)
-  discrete_age <- individual::IntegerVariable$new(initial_values = age0)
-  ab_titre <- individual::DoubleVariable$new(initial_values = ab_titre0)
-  exposure <- individual::TargetedEvent$new(population_size = n)
-
-  set.seed(1967391L)
-  inf_proc_nat <- infection_process_vaccine(parameters = parameters,variables = list(states=states,discrete_age=discrete_age,ab_titre=ab_titre),events = list(exposure=exposure),dt = dt)
-  inf_proc_nat(timestep = 100)
-
-  inf_R <- exposure$get_scheduled()$to_vector()
-
-  # C++
-  exposure <- individual::TargetedEvent$new(population_size = n)
-  set.seed(1967391L)
-  inf_proc_nat <- infection_process_vaccine_cpp(parameters = parameters,variables = list(states=states,discrete_age=discrete_age,ab_titre=ab_titre),events = list(exposure=exposure),dt = dt)
-  execute_process(process = inf_proc_nat,timestep = 100)
-
-  inf_cpp <- exposure$get_scheduled()$to_vector()
-
-  expect_equal(inf_R, inf_cpp)
-
-})
-
-test_that("R/C++ infection process consistent with NAT onward infectiousness and zero (-Inf) ab titre", {
-
-  library(nimue)
-
-  # pars
-  iso3c <- "GBR"
-  pop <- safir::get_population(iso3c)
-  contact_mat <- squire::get_mixing_matrix(iso3c = iso3c)
-  pop$n <- as.integer(pop$n / 100)
-
-  parameters <- safir::get_parameters(
-    population = pop$n,
-    contact_matrix_set = contact_mat,
-    iso3c = iso3c,
-    time_period = 365,
-    dt = 1
-  )
-  parameters$beta_set <- parameters$beta_set*rexp(n = length(parameters$beta_set))
-
-  # test states
-  n <- 1e5
-  dt <- 0.5
-  valid_states <- c("S","IMild","ICase","IAsymp")
-  state0 <- sample(x = valid_states,size = n,replace = T)
-  age0 <- sample.int(n = 17,size = n,replace = T)
-  vaccine_parameters <- get_vaccine_ab_titre_parameters(vaccine = "Pfizer")
-  parameters <- make_vaccine_parameters(safir_parameters = parameters,vaccine_ab_parameters = vaccine_parameters,vaccine_set = rep(100,365),dose_period = c(NaN, 10),strategy_matrix = nimue::strategy_matrix(strategy = "Elderly"),next_dose_priority_matrix = matrix(0,nrow = 1,ncol = 17))
-  parameters$nt_efficacy_transmission <- TRUE
-
-  ab_titre0 <- rep(-Inf, n)
-
-  # R
-  states <- individual::CategoricalVariable$new(categories = valid_states,initial_values = state0)
-  discrete_age <- individual::IntegerVariable$new(initial_values = age0)
-  ab_titre <- individual::DoubleVariable$new(initial_values = ab_titre0)
-  exposure <- individual::TargetedEvent$new(population_size = n)
-
-  set.seed(1967391L)
-  inf_proc_nat <- infection_process_vaccine(parameters = parameters,variables = list(states=states,discrete_age=discrete_age,ab_titre=ab_titre),events = list(exposure=exposure),dt = dt)
-  inf_proc_nat(timestep = 100)
-
-  inf_R <- exposure$get_scheduled()$to_vector()
-
-  # C++
-  exposure <- individual::TargetedEvent$new(population_size = n)
-  set.seed(1967391L)
-  inf_proc_nat <- infection_process_vaccine_cpp(parameters = parameters,variables = list(states=states,discrete_age=discrete_age,ab_titre=ab_titre),events = list(exposure=exposure),dt = dt)
-  execute_process(process = inf_proc_nat,timestep = 100)
-
-  inf_cpp <- exposure$get_scheduled()$to_vector()
-
-  expect_equal(inf_R, inf_cpp)
-
-})
-
