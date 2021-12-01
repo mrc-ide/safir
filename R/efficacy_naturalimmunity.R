98e8594df3dcd293dac05f3066fd301be43cceed
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-12-01 10:46:29 -0800

    fix bug with incorrect NAT decay

--- R/efficacy_naturalimmunity.R
+++ R/efficacy_naturalimmunity.R
@@ -31,7 +31,7 @@ natural_immunity_ab_titre_process <- function(parameters, variables, dt) {
         current_ab_titre <- variables$ab_titre$get_values(index = vaccinated_or_infected)
 
         # new Ab titre
-        new_ab_titre <- current_ab_titre + parameters$dr_vec[time_since_last_dose_or_infection]
+        new_ab_titre <- current_ab_titre + (parameters$dr_vec[time_since_last_dose_or_infection] * dt)
 
         # schedule an update
         variables$ab_titre$queue_update(values = new_ab_titre, index = vaccinated_or_infected)
